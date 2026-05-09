f7eded2083e4deb465cd1504c7145d32cec406c5
Sean Williams <sean@xeed.io>
2026-05-08 23:51:41 -0400

    fix: silence qwen xml import log

--- vllm/tool_parsers/qwen3xml_tool_parser.py
+++ vllm/tool_parsers/qwen3xml_tool_parser.py
@@ -1154,9 +1154,6 @@ class Qwen3XMLToolParser(ToolParser):
         self.prev_tool_call_arr: list[dict] = []
         self.streamed_args_for_tool: list[str] = []
 
-        logger.info(
-            "vLLM Successfully import tool parser %s !", self.__class__.__name__
-        )
 
     def extract_tool_calls(
         self,
